ecee212ecbfd7b85879d06ce5dc871c5c9aa60df
paolabel <pa0la.abel@hotmail.com>
2021-06-13 21:00:59 -0300

    sql

--- src/main/java/com/example/apidesafio/service/DBHandler.java
+++ src/main/java/com/example/apidesafio/service/DBHandler.java
@@ -27,7 +27,7 @@ public class DBHandler {
         String sql = "CREATE TABLE IF NOT EXISTS Certificates (\n"
                     +"serial_number	    INTEGER PRIMARY KEY NOT NULL,\n"
                     +"certificate		BLOB UNIQUE NOT NULL,\n"
-                    +"issuer_name 	    VARCHAR(30) NOT NULL,\n"
+                    +"name 	            VARCHAR(30) NOT NULL,\n"
                     +"not_before	 	VARCHAR(20) NOT NULL,\n"
                     +"not_after 		VARCHAR(20) NOT NULL\n"
                     +");";
@@ -44,7 +44,14 @@ public class DBHandler {
 
     public static boolean insert(X509Certificate certificate) {
         boolean isInserted = false;
-            
+        String sql = "INSERT INTO Certificates(serial_number, certificate, name,"
+                    +" not_before, not_after) VALUES (?, ?, ?, ?, ?)";
+        try {
+
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            exception.printStackTrace();
+        }
         return isInserted;
     }
 }
